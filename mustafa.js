9a8af67cd022078aa785b739254d35f859dce510
mustafa-ali-qbatch <mustafa.ali@qbatch.com>
2022-12-23 12:23:38 +0500

    testing

--- mustafa.js
+++ mustafa.js
@@ -1,5 +1,5 @@
-export const getValues = (key) => (array) =>
-  array.reduce((obj, item) => {
+export const getValues = (key) => (data) =>
+  data.reduce((obj, item) => {
     const [date] = Object.keys(item);
     const [month, keys] = [date.slice(4, 6), Object.keys(item)];
     obj[month] = Object.values(item[keys][key]);
